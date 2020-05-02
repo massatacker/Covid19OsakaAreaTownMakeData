4544edc5e109f812bbaaa9f8f7c5b1e26148a546
仲野政賢 <masataka@MacProLate2013HS.local>
2020-05-02 13:11:22 +0900

    2020-0502-1311

--- Covid19_OsakaTransDataNum7DaysDischarge.py
+++ Covid19_OsakaTransDataNum7DaysDischarge.py
@@ -12,6 +12,9 @@ source_filename = 'Coivd19_Osaka_Table.xlsx'
 trans_filename = 'OsakaAreaTownTransList.xlsx'
 area_output_filename = 'Covid19_Osaka_AreaNumData.xlsx'
 town_output_filename = 'Covid19_Osaka_TownNumData.xlsx'
+clmns_area = ['日付', '区域', '日別', '退院・解除', '週平均', '累計', '退院・解除累計']
+clmns_town = ['日付', '市町村', '日別', '退院・解除', '週平均', '累計', '退院・解除累計']
+mrk_discharge = '○'
 
 
 # In[ ]:
@@ -35,23 +38,33 @@ def add_up_num_infection( df_area, area, first_day, last_day, df_num ):
     infection_dates = df_area['日付'].unique()
     # 感染日の一覧を昇順に並べ換える
     infection_dates.sort()
-    # 累計人数計算用の変数をクリアする
+    # 陽性者の累計人数計算用の変数をクリアする
     num_total_infection_people = 0
+    # 退院・解除者の累計人数計算用の変数をクリアする
+    num_total_discharge_people = 0
     # 感染日初日から最終日までループする
     #first_day = pd.to_datetime(infection_dates[0])
     #last_day = pd.to_datetime(infection_dates[-1])
     for infection_date in daterange_p1(first_day, last_day):
-        # 感染日毎の人数を求める
-        num_infection_people = len(df_area[ df_area['日付']==infection_date ])
-        # 累計人数を求める
+        # 感染日毎のデータを抽出する
+        df_area_date = df_area[ df_area['日付']==infection_date ]
+        # 感染日毎の陽性者の人数を求める
+        num_infection_people = len(df_area_date)
+        # 陽性者の累計人数を求める
         num_total_infection_people += num_infection_people
+        # 感染日毎の退院・解除者の人数を求める
+        num_discharge_people = len(df_area_date[ df_area_date['退院・解除']==mrk_discharge ])
+        # 退院・解除者の累計人数を求める
+        num_total_discharge_people += num_discharge_people
         # [感染日、居住地、感染日の人数]のSeriesを作成する
         s_infection = pd.Series(
                             [infection_date, 
                              area,
                              num_infection_people,
+                             num_discharge_people,
                              0,
-                             num_total_infection_people],
+                             num_total_infection_people,
+                             num_total_discharge_people],
                              index = df_num.columns)
         # 作成したSeriesをdf_numに追加する
         df_num = df_num.append(s_infection, ignore_index=True)
@@ -65,8 +78,8 @@ def add_up_num_infection( df_area, area, first_day, last_day, df_num ):
 
 
 #集計結果を格納するためのDataFrameを宣言する
-df_areas_num = pd.DataFrame(columns=['日付', '区域', '日別', '週平均', '累計'])
-df_towns_num = pd.DataFrame(columns=['日付', '市町村', '日別', '週平均', '累計'])
+df_areas_num = pd.DataFrame(columns=clmns_area)
+df_towns_num = pd.DataFrame(columns=clmns_town)
 
 # 元データを読み込む
 df_source = pd.read_excel(source_filename)
@@ -121,7 +134,7 @@ for area in areas:
     # 区域単位のデータを抽出する
     df_area = df_source[ df_source['区域']==area ]
     # 区域単位の集計結果を格納するためのデータフレームを宣言する
-    df_area_num = pd.DataFrame(columns=['日付', '区域', '日別', '週平均', '累計'])
+    df_area_num = pd.DataFrame(columns=clmns_area)
     # 集計する
     df_area_num = add_up_num_infection( df_area, area, first_day, last_day, df_area_num )
     # 区域全体の集計結果に追加格納する
@@ -129,7 +142,7 @@ for area in areas:
 # 全域で集計する(おまけ)
 print('\r全域        ', end='')
 # 区域単位の集計結果を格納するためのデータフレームを宣言する
-df_area_num = pd.DataFrame(columns=['日付', '区域', '日別', '週平均', '累計'])
+df_area_num = pd.DataFrame(columns=clmns_area)
 # 集計する
 df_area_num = add_up_num_infection( df_source, '全域', first_day, last_day, df_area_num )
 # 区域全体の集計結果に追加格納する
@@ -146,7 +159,7 @@ for town in towns:
     # 市町村単位のデータを抽出する
     df_town = df_source[ df_source['市町村']==town ]
     # 市町村単位の集計結果を格納するためのデータフレームを宣言する
-    df_town_num = pd.DataFrame(columns=['日付', '市町村', '日別', '週平均', '累計'])
+    df_town_num = pd.DataFrame(columns=clmns_town)
     # 集計する
     df_town_num = add_up_num_infection( df_town, town, first_day, last_day, df_town_num )
     # 区域全体の集計結果に追加格納する
